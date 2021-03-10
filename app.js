7109fd00aaa2974965bb72c9069f68df55be6fd3
Harsh <harshvardhan.mec18@itbhu.ac.in>
2021-03-10 17:40:33 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//jshint esversion:6
-require("dotenv").config();
+
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -8,28 +8,28 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+const findOrCreate = require('mongoose-findorcreate');
+
 
 const app = express();
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+
 app.use(session({
-  secret: "Thisisalittlesecret",
+  secret: "Thisisthesecretforsecret.",
   resave: false,
   saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.set("useCreateIndex", true);
-mongoose.connect("mongodb://localhost:27017/userDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect("mongodb+srv://admin-harsh:Test123@cluster0.b8xbg.mongodb.net/confessionDB", {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
   email: String,
@@ -62,6 +62,7 @@ const Truth = new mongoose.model("Truth", typeSchema);
 const Experience = new mongoose.model("Experience", typeSchema);
 const Other = new mongoose.model("Other", typeSchema);
 
+
 passport.use(User.createStrategy());
 
 passport.serializeUser(function(user, done) {
@@ -74,82 +75,51 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
+
 passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({
-      googleId: profile.id
-    }, function(err, user) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
 ));
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get("/auth/google", passport.authenticate("google", {
-  scope: ["profile"]
-}));
-
-app.get("/auth/google/secrets",
-  passport.authenticate("google", {
-    failureRedirect: "/login"
-  }),
-  function(req, res) {
-    res.redirect("/secrets");
-  }
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ["profile"] })
 );
 
-app.get("/login", function(req, res) {
-  res.render("login");
-});
-
-app.get("/register", function(req, res) {
-  res.render("register");
-});
-
-app.get("/secrets", function(req, res) {
-  User.find({
-    "ofString": {
-      $ne: null
+app.get( '/auth/google/secrets',
+    passport.authenticate( 'google', {failureRedirect: '/login'}),
+    function(req, res){
+      res.redirect("/secrets");
     }
-  }, function(err, foundUsers) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUsers) {
-        res.render("secrets", {
-          userWithSecrets: foundUsers
-        });
-      }
-    }
-  });
-});
-
-app.get("/submit", function(req, res) {
-  if (req.isAuthenticated()) {
-    res.render("submit");
-  } else {
-    res.redirect("/login");
-  }
-});
-
-app.get("/privacy", function(req, res) {
+);
+app.get("/privacy", function(req, res){
   res.render("privacy");
 });
-
-app.get("/about", function(req, res) {
+app.get("/about", function(req, res){
   res.render("about");
-});
+})
 
-app.get("/dream", function(req, res) {
-  Dream.find({}, function(err, dreams) {
+app.get("/login", function(req, res){
+  res.render("login");
+})
+app.get("/register", function(req, res){
+  res.render("register");
+})
+
+app.get("/dream", function(req, res){
+  Dream.find({}, function(err, dreams){
     res.render("type", {
       message: dreams,
       type: "A Dream",
@@ -159,8 +129,8 @@ app.get("/dream", function(req, res) {
   });
 });
 
-app.get("/fantasy", function(req, res) {
-  Fantasy.find({}, function(err, fantasies) {
+app.get("/fantasy", function(req, res){
+  Fantasy.find({}, function(err, fantasies){
     res.render("type", {
       message: fantasies,
       type: "A Fantasy",
@@ -170,8 +140,8 @@ app.get("/fantasy", function(req, res) {
   });
 });
 
-app.get("/guilt", function(req, res) {
-  Guilt.find({}, function(err, guilts) {
+app.get("/guilt", function(req, res){
+  Guilt.find({}, function(err, guilts){
     res.render("type", {
       message: guilts,
       type: "A Guilt",
@@ -181,8 +151,8 @@ app.get("/guilt", function(req, res) {
   });
 });
 
-app.get("/lie", function(req, res) {
-  Lie.find({}, function(err, lies) {
+app.get("/lie", function(req, res){
+  Lie.find({}, function(err, lies){
     res.render("type", {
       message: lies,
       type: "A Lie",
@@ -192,8 +162,8 @@ app.get("/lie", function(req, res) {
   });
 });
 
-app.get("/truth", function(req, res) {
-  Truth.find({}, function(err, truths) {
+app.get("/truth", function(req, res){
+  Truth.find({}, function(err, truths){
     res.render("type", {
       message: truths,
       type: "A Truth",
@@ -203,8 +173,8 @@ app.get("/truth", function(req, res) {
   });
 });
 
-app.get("/experience", function(req, res) {
-  Experience.find({}, function(err, experiences) {
+app.get("/experience", function(req, res){
+  Experience.find({}, function(err, experiences){
     res.render("type", {
       message: experiences,
       type: "A Wild Experience",
@@ -214,116 +184,100 @@ app.get("/experience", function(req, res) {
   });
 });
 
-app.get("/other", function(req, res) {
-  Other.find({}, function(err, others) {
+app.get("/other", function(req, res){
+  Other.find({}, function(err, others){
     res.render("type", {
       message: others,
       type: "Other Emotions",
       typeword: "Priceless Emotions",
       image: "https://simplyconfess.com/wp-content/themes/list-mag-wp-child/img/icons/a-random-feeling.png"
     });
+    console.log(others);
   });
 });
 
-app.get("/logout", function(req, res) {
-  req.logout();
-  res.redirect("/");
-});
-
-app.post("/register", function(req, res) {
-  User.register({
-    username: req.body.username
-  }, req.body.password, function(err, user) {
-    if (err) {
-      console.log(err);
-      res.redirect("/register");
-    } else {
-      passport.authenticate("local")(req, res, function() {
-        res.redirect("/secrets");
-      });
-    }
-  });
-
-});
-
-app.post("/login", function(req, res) {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
-  req.login(user, function(err) {
-    if (err) {
+app.get("/secrets", function(req, res){
+  User.find({"ofString": {$ne: null}}, function(err, foundUsers){
+    if(err){
       console.log(err);
     } else {
-      passport.authenticate("local")(req, res, function() {
-        res.redirect("/secrets");
-      });
+      if(foundUsers){
+        res.render("secrets", {userWithSecrets: foundUsers});
+      }
     }
-  });
-});
+  })
+})
 
-app.post("/submit", function(req, res) {
+app.get("/submit", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+})
+
+app.post("/submit", function(req, res){
   const submittedSecret = req.body.secret;
   const type = req.body.type;
   console.log(req.user.id);
-  User.findById(req.user.id, function(err, foundUser) {
-    if (err) {
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
       console.log(err);
     } else {
-      if (foundUser) {
+      if(foundUser){
         foundUser.ofString.push(submittedSecret);
-        if (type === "dream") {
+        if(type === "dream"){
           const dream = new Dream({
             text: submittedSecret
           });
-          dream.save(function(err) {
-            if (!err) {
+          dream.save(function(err){
+            if(!err){
               console.log(Dream);
             }
           });
 
-        } else if (type === "fantasy") {
+        } else if(type === "fantasy"){
           const fantasy = new Fantasy({
             text: submittedSecret
           });
-          fantasy.save(function(err) {
-            if (!err) {
+          fantasy.save(function(err){
+            if(!err){
               console.log(Fantasy);
             }
           });
-        } else if (type === "guilt") {
+        } else if(type === "guilt"){
           const guilt = new Guilt({
             text: submittedSecret
           });
-          guilt.save(function(err) {
-            if (!err) {
+          guilt.save(function(err){
+            if(!err){
               console.log(Guilt);
             }
           });
-        } else if (type === "lie") {
+        } else if(type === "lie"){
           const lie = new Lie({
             text: submittedSecret
           });
-          lie.save(function(err) {
-            if (!err) {
+          lie.save(function(err){
+            if(!err){
               console.log(Lie);
             }
           });
-        } else if (type === "truth") {
+        } else if(type === "truth"){
           const truth = new Truth({
             text: submittedSecret
           });
-          truth.save(function(err) {
-            if (!err) {
+          truth.save(function(err){
+            if(!err){
               console.log(Truth);
             }
           });
-        } else if (type === "experience") {
+        } else if(type === "experience"){
           const experience = new Experience({
             text: submittedSecret
           });
-          experience.save(function(err) {
-            if (!err) {
+          experience.save(function(err){
+            if(!err){
               console.log(Experience);
             }
           });
@@ -331,13 +285,13 @@ app.post("/submit", function(req, res) {
           const other = new Other({
             text: submittedSecret
           });
-          other.save(function(err) {
-            if (!err) {
+          other.save(function(err){
+            if(!err){
               console.log(Other);
             }
           });
         }
-        foundUser.save(function() {
+        foundUser.save(function(){
           res.redirect("/secrets");
         });
       }
@@ -345,8 +299,40 @@ app.post("/submit", function(req, res) {
   });
 });
 
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+})
 
+app.post("/register", function(req, res){
+   User.register({username: req.body.username}, req.body.password, function(err, user){
+     if(err){
+       console.log(err);
+       res.render("register");
+     } else {
+       passport.authenticate("local")(req, res, function(){
+         res.redirect("/secrets");
+       })
+     }
+   })
+});
+
+app.post("/login", function(req, res){
+   const user = new User({
+     username: req.body.username,
+     password: req.body.password
+   });
+   req.login(user, function(err){
+     if(err){
+       console.log(err);
+     } else {
+       passport.authenticate("local")(req, res, function(){
+         res.redirect("/secrets");
+       });
+     }
+   });
+});
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started successfully.");
+  console.log("Server has started successfully");
 });
